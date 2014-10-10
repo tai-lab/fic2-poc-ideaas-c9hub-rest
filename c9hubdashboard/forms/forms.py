a12fa2d5a948054fccc7a38c96e2afac5b81a31b
geoffroy <you@example.com>
2014-10-10 18:22:20 +0200

    Putting some css style on the root page

--- c9hubdashboard/forms/forms.py
+++ c9hubdashboard/forms/forms.py
@@ -9,7 +9,7 @@ class BaseForm(Form):
 
 
 class CreateIdeForm(BaseForm):
-    display_name = StringField('Display name', validators=[DataRequired(), Length(min=1, max=64)], default='toto')
-    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=32)], default='toto')
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=1, max=32)], default='toto')
+    display_name = StringField('Display name', validators=[DataRequired(), Length(min=1, max=64)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=32)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=1, max=32)])
     git_clones = StringField('Git clones', validators=[DataRequired()], default='https://github.com/mk-fg/layered-yaml-attrdict-config.git https://github.com/kmike/port-for.git')
